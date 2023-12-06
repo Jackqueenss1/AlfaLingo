070e98aa10c900d50f4316822dcf1d5d9f4b95cc
Jackqueenss1 <152095139+Jackqueenss1@users.noreply.github.com>
2023-12-06 21:46:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -59,78 +59,77 @@ function playSound10(){
     snd.play()
 }
 function playSound11(){
-    var snd = new Audio('A.mp3')//wav is also supported
+    var snd = new Audio('K.mp3')//wav is also supported
     snd.play()//plays the sound
 }
 function playSound12(){
-    var snd = new Audio('B.mp3')
+    var snd = new Audio('L.mp3')
     snd.play()
 }
 function playSound13(){
-    var snd = new Audio('C.mp3')
+    var snd = new Audio('M.mp3')
     snd.play()
 }
 function playSound14(){
-    var snd = new Audio('D.mp3')
+    var snd = new Audio('N.mp3')
     snd.play()
 }
 function playSound15(){
-    var snd = new Audio('E.mp3')
+    var snd = new Audio('O.mp3')
     snd.play()
 }
 function playSound16(){
-    var snd = new Audio('F.mp3')//wav is also supported
+    var snd = new Audio('P.mp3')//wav is also supported
     snd.play()//plays the sound
 }
 function playSound17(){
-    var snd = new Audio('G.mp3')
+    var snd = new Audio('Q.mp3')
     snd.play()
 }
 function playSound18(){
-    var snd = new Audio('H.mp3')
+    var snd = new Audio('R.mp3')
     snd.play()
 }
 function playSound19(){
-    var snd = new Audio('I.mp3')
+    var snd = new Audio('S.mp3')
     snd.play()
 }
 function playSound20(){
-    var snd = new Audio('J.mp3')
+    var snd = new Audio('T.mp3')
     snd.play()
 }
 function playSound21(){
-    var snd = new Audio('A.mp3')//wav is also supported
+    var snd = new Audio('Ump3')//wav is also supported
     snd.play()//plays the sound
 }
 function playSound22(){
-    var snd = new Audio('B.mp3')
+    var snd = new Audio('V.mp3')
     snd.play()
 }
 function playSound23(){
-    var snd = new Audio('C.mp3')
+    var snd = new Audio('W.mp3')
     snd.play()
 }
 function playSound24(){
-    var snd = new Audio('D.mp3')
+    var snd = new Audio('X.mp3')
     snd.play()
 }
-function playSound25(){
-    var snd = new Audio('E.mp3')
+function playSound25(){Y.mp3')
     snd.play()
 }
 function playSound26(){
-    var snd = new Audio('F.mp3')//wav is also supported
+    var snd = new Audio('Z.mp3')//wav is also supported
     snd.play()//plays the sound
 }
 function playSound27(){
-    var snd = new Audio('G.mp3')
+    var snd = new Audio('Æ.mp3')
     snd.play()
 }
 function playSound28(){
-    var snd = new Audio('H.mp3')
+    var snd = new Audio('Ø.mp3')
     snd.play()
 }
 function playSound29(){
-    var snd = new Audio('I.mp3')
+    var snd = new Audio('Å.mp3')
     snd.play()
 }
